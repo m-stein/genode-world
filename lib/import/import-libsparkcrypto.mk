e6e96af604b0ebdbbcb9af75f56e087ccb0ca2c3
Martin Stein <martin.stein@genode-labs.com>
2019-04-04 19:39:49 +0200

    WIP import-libsparkcrypto: try add missing SRC_ADS

--- lib/import/import-libsparkcrypto.mk
+++ lib/import/import-libsparkcrypto.mk
@@ -1,5 +1,11 @@
 LSC_DIR := $(call select_from_ports,libsparkcrypto)/libsparkcrypto/src
 
+SRC_ADS += lsc.ads
+#SRC_ADS += lsc-aes.ads
+
+vpath lsc.ads     $(LSC_DIR)/shared/generic
+#vpath lsc-aes.ads $(LSC_DIR)/shared/generic
+
 INC_DIR += $(LSC_DIR)/ada/generic \
            $(LSC_DIR)/shared/little_endian \
            $(LSC_DIR)/shared/generic \
